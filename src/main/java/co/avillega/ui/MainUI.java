7ea6bd4f1c789c2037e051f4c88924e839174806
avillega <villegas.pelaez.andres@gmail.com>
2017-04-05 08:03:16 -0500

    Added panel and navigation

--- src/main/java/co/avillega/ui/MainUI.java
+++ src/main/java/co/avillega/ui/MainUI.java
@@ -1,70 +1,62 @@
 package co.avillega.ui;
 
 
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.shared.ui.slider.SliderOrientation;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.*;
-import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringUI
 public class MainUI extends UI{
 
-
-    private VerticalLayout root;
+    public static final String CONTROLVIEW = "";
+    public static final String RUTINASVIEW = "rutinas";
+    AuxPanel auxPanel;
+    @Autowired
+    ControlPanel controlPanel;
+    @Autowired
+    RutinasPanel rutinasPanel;
+    VerticalLayout contentLayout;
+    Navigator navigator;
+    private GridLayout root;
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         setupLayout();
-        addHeader();
-        addControlSlider();
-        addActionButtons();
+
     }
 
-
-
-
     private void setupLayout() {
-        root = new VerticalLayout();
-        root.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        auxPanel = new AuxPanel(this);
+        contentLayout = new VerticalLayout();
+
+
+        root = new GridLayout(2, 1);
+
+        navigator = new Navigator(this, contentLayout);
+        navigator.addView(CONTROLVIEW, controlPanel);
+        navigator.addView(RUTINASVIEW, rutinasPanel);
+
+
+        root.setColumnExpandRatio(0, 0.3f);
+        root.setColumnExpandRatio(1, 1);
+
+        root.setSizeFull();
+        root.addComponents(auxPanel);
+        root.addComponent(contentLayout);
+
+
+
+
         setContent(root);
     }
 
-    private void addHeader() {
-        VerticalLayout headerLayout = new VerticalLayout();
-        headerLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
-
-        Label header = new Label("Automata");
-
-        header.addStyleName(ValoTheme.LABEL_H1);
-        Label subtitle = new Label("Control de la banda transportadora de la universidad Icesi");
-        subtitle.addStyleName(ValoTheme.LABEL_H4);
-        headerLayout.addComponents(header, subtitle);
-        root.addComponent(headerLayout);
+    public Navigator getNavigator() {
+        return navigator;
     }
 
-    private void addControlSlider(){
-        VerticalLayout controlLayout = new VerticalLayout();
-        controlLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
-        Label label = new Label("0 m/s");
-        label.addStyleName(ValoTheme.LABEL_H3);
-        Slider control = new Slider(0, 10, 1);
-        control.setCaption("Control");
-        control.setWidth("50%");
-        control.setOrientation(SliderOrientation.HORIZONTAL);
-        control.addValueChangeListener(event -> {label.setValue(control.getValue()+" m/s");});
-        controlLayout.addComponents(label);
-        controlLayout.addComponentsAndExpand(control);
-        root.addComponent(controlLayout);
-
-
-    }
-
-    private void addActionButtons() {
-
-        //Todo add action buttons (Stop, start and emergency)
-    }
 }
